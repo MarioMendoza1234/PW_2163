120b246734ced30e8370e2f1891d170f1657fc2e
MarioMendoza1234 <jorge.mario.2306@gmail.com>
2016-11-07 11:53:48 -0700

    calculadora terminada

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,43 +1,46 @@
-//variable global
+//Variable global.
 var operador = "";
-function operadores(ope) //+, -. *, /
+
+function operadores(ope) //+, -, *, /
 {
-	operador = ope
+	operador = ope;
 }
+
 function igual()
 {
 	var valor1 = document.calculadora.operando1.value;
 	var valor2 = document.calculadora.operando2.value;
-	var resultado = 0;
-	if (operador == "+")
-		resultado = valor1 + valor2;
-	else if(operador == "-")
-		resultado = valor1 - valor2;
-	else if(operador == "*")
-		resultado = valor1 * valor2;
-	else if(operador == "/")
-		resultado = valor1 / valor2;
+
 	document.calculadora.resultado.value = eval(valor1+operador+valor2);
+}
+function limpiar()
+{
+	operador = ""
+	document.calculadora.operando1.value=0;
+	document.calculadora.operando2.value=0;
+	document.calculadora.resultado.value=0;
+
 }
 function numeros(num)
 {
-	if(operador == "")  //escribir en el operadndo1
+	if(operador == "") //Escribir en el operando1
 	{
-	//guardamos el valor del operando1
+		//Guardamos el valor del operando1
 		var valor = document.calculadora.operando1.value;
-		if(valor=="0")
-			{
-				document.calculadora.operando1.value = ""
-			}
+		if(valor == "0")
+		{
+			document.calculadora.operando1.value = "";
+		}
 		document.calculadora.operando1.value = document.calculadora.operando1.value + num;
 	}
-	else //escribir en el operando2
+	else //Escribir en el operando2
 	{
+		//Guardamos el valor del operando2
 		var valor = document.calculadora.operando2.value;
 		if(valor == "0")
-			{
-				document.calculadora.operando2.value = ""
-			}
-		document.calculadora.operando2.valuue = document.calculadora.operando2.value + num;
+		{
+			document.calculadora.operando2.value = "";
+		}
+		document.calculadora.operando2.value = document.calculadora.operando2.value + num;
 	}
 }
