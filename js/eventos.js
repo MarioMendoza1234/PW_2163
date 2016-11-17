85de27f224f7cc0317545df399613968f691ce09
MarioMendoza1234 <jorge.mario.2306@gmail.com>
2016-11-17 11:52:38 -0700

    finaliza JQuery

--- js/eventos.js
+++ js/eventos.js
@@ -1,19 +1,23 @@
 // DOM = Modelo de objetos del documento
 var inicio = function() //main
 {
-	var dameclick = function()
+	var dameclic = function()
 	{
 		$.ajax({
-  			url: 'https://randomuser.me/api/',
-  			dataType: 'json',
-  			success: function(data) {
-   			 console.log(data.results[0].name.first+" "+data.results[0].name.last);
-  			}
-		});  
+		  url: 'https://randomuser.me/api/',
+		  dataType: 'json',
+		  success: function(data) {
+		  	$("imgFoto").show("slow");
+		  	$("txtNombre").show("slow");
+		  	$("#txtNombre").val(data.results[0].name.first+" "+data.results[0].name.last);
+		    $("#imgFoto").attr("src",data.results[0].picture.large);
+		    $("#miArticle").html("Texto");
+		    console.log(data.results[0].name.first+" "+data.results[0].name.last);
+		  }
+		});
 	}
-	$("#dameClick").on("click",dameclick);
+	$("#dameClic").on("click",dameclic);
 }
 
-
-//Inicializar nuestro documento
+//Inicializar nuestro Documento
 $(document).on("ready",inicio);
